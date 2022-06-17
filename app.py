f43d76281bb67651048caccdfabb7aed892fcbbb
Jahaira Marrero <jahaira.marrero@gmail.com>
2022-06-16 23:34:25 -0400

    syntax

--- app.py
+++ app.py
@@ -29,10 +29,10 @@ midpoint = (np.average(data['lat']), np.average(data['lon']))
 st.pydeck_chart(pdk.Deck(
            map_style="mapbox://styles/mapbox/light-v9",
            initial_view_state=pdk.ViewState(
-                      "latitude": midpoint[0],
-                      "longitude": midpoint[1],
-                      "zoom": 11,
-                      "pitch": 50,
+                      latitude: midpoint[0],
+                      longitude: midpoint[1],
+                      zoom: 11,
+                      pitch: 50,
            ),
            layers = [
                       pdk.Layer(
