f66a132b4153166f787637e322b2fc6fef1c29db
Jahaira Marrero <jahaira.marrero@gmail.com>
2022-06-16 21:11:35 -0400

    format date

--- app.py
+++ app.py
@@ -16,5 +16,8 @@ if st.button('Get List'):
         data = pd.read_sql("select * from trips limit 100;", conn)
         st.write(data)
 
+st.subheader('Hourly Statistics')
 df = pd.read_sql(f'select date_trunc("hour", starttime) as "date", count(*) as"num_trips", avg(tripduration)/60 as "avg duration (mins)", avg(haversine(start_station_latitude, start_station_longitude, end_station_latitude,end_station_longitude)) as "avg distance (km)" from trips group by 1 order by 1;', conn)
+df['date'] = pd.to_datetime(df['date'])
+
 st.write(df)
