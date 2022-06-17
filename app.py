cf8d6ef00b0bfd07d89b5bb4bc6165a31aa41e16
Jahaira Marrero <jahaira.marrero@gmail.com>
2022-06-16 20:31:18 -0400

    snowflake import

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import snowflake
 import snowflake.connector
 import pandas as pd
 
 st.title('Citibike Dashboard')
+
 def get_citi_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from trips where starttime='2018-06-21 08:06:24.076'")
