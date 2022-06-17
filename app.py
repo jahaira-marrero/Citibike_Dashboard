59e53d6e4379b7ced6e0da6db51fb3571104e8cd
Jahaira Marrero <jahaira.marrero@gmail.com>
2022-06-16 20:22:23 -0400

    snowflake connection

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import snowflake.connector
 
 st.title('Citibike Dashboard')
+def get_citi_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from trips where starttime='2018-06-21 08:06:24.076'")
+        return my_cur.fetchall()
+
+if st.button('Get List'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_data_rows = get_citi_list()
+    my_cnx.close()
+    st.dataframe(my_data_rows)
